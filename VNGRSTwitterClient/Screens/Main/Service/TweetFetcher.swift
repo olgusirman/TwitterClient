11e1d12bac4d4b4bccf904c4e1c563c3e1c022a8
Olgu Sırman <olgu.sirman@vngrs.com>
2018-10-08 18:45:42 +0300

    Fetcher implemented

--- VNGRSTwitterClient/Screens/Main/Service/TweetFetcher.swift
+++ VNGRSTwitterClient/Screens/Main/Service/TweetFetcher.swift
@@ -9,11 +9,56 @@
 import Foundation
 import Alamofire
 
+
+
+
 // Generic Fetcher protocol
-protocol Fetcher {
-    associatedtype FetcherElement: Any
-    typealias completionHandler = (FetcherElement?, _ dataResponse: DataResponse<Any>, Swift.Error?) -> Void
-    func search( searchRouterObject: SearchRouterObject, completionHandler : @escaping completionHandler) //TODO: Generic bir router object
+protocol Fetcher: class {
+    typealias completionHandler<FetcherElement> = (FetcherElement?, _ dataResponse: DataResponse<Any>, Swift.Error?) -> Void
+    func fetch<FetcherElement>( convertible: URLRequestConvertible, completionHandler : @escaping completionHandler<FetcherElement>) //TODO: Generic bir router object
+}
+
+class FetcherModel: Fetcher {
+   
+    // MARK: Private Initializer property
+    private let networking: AlamoNetworking
+    
+    // MARK: Initializer
+    init(networking: AlamoNetworking) {
+        self.networking = networking
+    }
+    
+    func fetch<FetcherElement>(convertible: URLRequestConvertible, completionHandler : @escaping completionHandler<FetcherElement>) {
+
+        networking.request(use:convertible).responseJSON { (dataResponse) in
+            
+            // Handle generic error
+            ErrorManager.error(with: dataResponse)
+            
+            switch dataResponse.result {
+            case .success:
+                
+                if let fetcher = dataResponse.value as? FetcherElement {
+                    completionHandler(fetcher, dataResponse,nil)
+                } else {
+                    let userInfo = [NSLocalizedDescriptionKey: "dataResponse FetcherElement cast error"]
+                    let fetcherCastError = NSError(domain: "", code: 409, userInfo: userInfo)
+                    completionHandler(nil, dataResponse, fetcherCastError)
+                }
+                
+                // If you use class, you can use Resolvable protocol also. But generally codable usage is OK, so that reason no need to Object Mapper a lot.
+//                if let object = Resolver.resolve(resolvedObject: BaseObject.self, dataResponse: dataResponse) {
+//                    completionHandler(object.statuses, dataResponse, nil)
+//                }
+                
+            case .failure(let error):
+                completionHandler(nil, dataResponse, error)
+            }
+            
+        }
+        
+    }
+    
 }
 
 // TODO: later use generic fetcher protocol to use TweetFetcher struct
@@ -22,8 +67,8 @@ protocol TweetFetcher {
     func search(searchRouterObject: SearchRouterObject, completionHandler : @escaping handler)
 }
 
-struct MasterViewControllerTweetFetcherViewModel: Fetcher {
-        
+struct MasterViewControllerTweetFetcherViewModel: TweetFetcher {
+    
     // MARK: Private Initializer property
     private let networking: AlamoNetworking
     
